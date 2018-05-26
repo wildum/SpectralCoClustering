474f5946a0757f5c7a52fb0072539f7fe675b931
wildum <helmutlbb@orange.fr>
2018-05-27 00:32:04 +0200

    custom coclustering with poor results

---
+++ custom_spectralclustering.py
@@ -0,0 +1,41 @@
+from scipy.sparse import csgraph
+from sklearn.cluster import KMeans
+import numpy as np
+
+def custom_spectral_custering(A,k):
+
+    #apply laplacian
+    L = csgraph.laplacian(A, normed=True)
+
+    #compute eigenvectors and eigenvalues
+    eigvals, eigvects = np.linalg.eig(L)
+
+    #take the k lowest eigen values
+    eigvals_sorted = sorted(eigvals)[:k]
+
+    #get indexes of these values in the original list
+    idx = [eigvals.tolist().index(el) for el in eigvals_sorted]
+
+    #get k eigen vectors corresponding to the k eigen values
+    k_eigvects = [eigvects[i] for i in idx]
+
+    #build matrix of k eigenvectors
+    mat_k_eigvects = np.transpose(np.asmatrix(k_eigvects))
+
+    R = L.dot(mat_k_eigvects)
+
+    #apply k means
+    kmeans = KMeans(n_clusters=5, random_state=0).fit(R)
+    kmeans_result = kmeans.fit_predict(R)
+
+    #matrix of clusters
+    clusters = [[] for i in range(k)]
+
+    #feed result with A rows to build clusters
+    for i in range(len(A)):
+        clusters[kmeans_result[i]].append(A[i])
+    
+    #merge clusters to create a matrix
+    result = [item for sublist in clusters for item in sublist]
+
+    return result
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from sklearn.datasets import samples_generator as sg
 from sklearn.cluster.bicluster import SpectralCoclustering
 from sklearn.metrics import consensus_score
 
-data, rows, columns = make_biclusters(
-    shape=(300, 300), n_clusters=5, noise=5,
-    shuffle=False, random_state=0)
+import custom_spectralclustering as cs
+
+data, rows, columns = make_biclusters(shape=(30, 30), n_clusters=5, noise=5,shuffle=False, random_state=0)
+
+data = np.absolute(data)
 
 plt.matshow(data, cmap=plt.cm.Blues)
 plt.title("Original dataset")
@@ -24,10 +26,19 @@ score = consensus_score(model.biclusters_,
 
 print("consensus score: {:.3f}".format(score))
 
+custom_spectralclustering_result = cs.custom_spectral_custering(data, 5)
+custom_spectralcoclustering_result = cs.custom_spectral_custering(np.transpose(custom_spectralclustering_result), 5)
+
 fit_data = data[np.argsort(model.row_labels_)]
 fit_data = fit_data[:, np.argsort(model.column_labels_)]
 
 plt.matshow(fit_data, cmap=plt.cm.Blues)
 plt.title("After biclustering; rearranged to show biclusters")
+
+plt.matshow(custom_spectralclustering_result, cmap=plt.cm.Blues)
+plt.title("Custom spectral clustering")
+
+plt.matshow(custom_spectralcoclustering_result, cmap=plt.cm.Blues)
+plt.title("Custom spectral coclustering")
 
 plt.show()
